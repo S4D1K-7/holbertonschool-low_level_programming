0768888e09028f4b6a02474123b995af37696d43
niaz <niazshadiq@gmail.com>
2023-09-25 11:20:12 -0700

    test

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/*
+ * print_alphabet - it prints alphabet in lowercase.
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char k;
+
+	for (k = 'a' ; k <= 'z' ; k++)
+		_putchar(k);
+	_putchar('\n');
+}
